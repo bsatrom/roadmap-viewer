5f7a31214c30d7e1970ccf8ffdc4124617591ca6
Brandon Satrom <bsatrom@gmail.com>
2014-09-29 16:20:10 -0500

    added auth screen

---
+++ app/scripts/aha.js
@@ -0,0 +1,13 @@
+app.aha = (function () {
+    var client_id = '1d6653c08451fbfcebb340183630da718bc0138b957dcd187969ca8185486c66',
+      redirect_uri = 'urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob',
+      auth_url_base = 'https://secure.aha.io/oauth/authorize',
+      response_type = 'token';
+
+    var win;
+    return {
+        auth_url: auth_url_base + '?client_id=' + client_id +
+          '&redirect_uri=' + redirect_uri + '&response_type=' +
+          response_type
+    }
+})();
--- app/scripts/home.js
+++ app/scripts/home.js
@@ -1,6 +1,11 @@
 app.models.home = (function () {
     var title = 'home';
+    var win;
     return {
+        connectAha: function() {
+          win = window.open(app.aha.auth_url, '_blank', 'location=yes');
+        },
+
         title: title
     }
 })();
